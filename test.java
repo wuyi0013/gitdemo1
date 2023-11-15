0885ec91c69c4fd5c0b8070b174d903833531dbf
wuyi0013 <2676204234@qq.com>
2023-11-15 16:33:34 +0800

    te

---
+++ test.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: test
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author: 杨怀志
+ * @Create: 2023/11/15 - 16:33
+ * @Version: v1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
